3a33ec8a9b2ab9a05377dbc6f0087926539d9251
Wuodan <stefan.kuhn.zurich@gmail.com>
2020-07-16 21:56:43 +0200

    Shellcheck: fix useless cat

--- scripts/find-username.sh
+++ scripts/find-username.sh
@@ -4,7 +4,7 @@
 # input is json from stdin
 # output is the user name or 'pentoo' if it's not set in the config file
 # example usage:
-# cat config-file.json | ./find-username.sh
+# < config-file.json ./find-username.sh
 # example output:
 # vagrant
 
